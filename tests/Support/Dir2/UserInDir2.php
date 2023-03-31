1c5896f137b9c9e1d0eb47da1eb0c6b92b6babac
Dmitrii Derepko <xepozz@list.ru>
2023-03-31 23:48:45 +0300

    Add tests

---
+++ tests/Support/Dir2/UserInDir2.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Classifier\Tests\Support\Dir2;
+
+use Yiisoft\Classifier\Tests\Support\Interfaces\UserInterface;
+
+class UserInDir2 implements UserInterface
+{
+}
